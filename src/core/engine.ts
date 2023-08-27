a9bc54e18f79dac1174ec43de867b54ab0307eec
Pionxzh <pionxzh@csie.io>
2023-08-27 23:46:35 +0800

    fix: update useragent to Chrome 116

--- src/core/engine.ts
+++ src/core/engine.ts
@@ -13,7 +13,7 @@ export class Engine {
         this.request.setHeader('Host', this.BASE_URL.replace('https://', ''))
         this.request.setHeader('Origin', this.BASE_URL)
         this.request.setHeader('Referer', `${this.BASE_URL}/`)
-        this.request.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36')
+        this.request.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36')
 
         this.request.setCookie('platform', 'pc')
         // bypass age confirmation
